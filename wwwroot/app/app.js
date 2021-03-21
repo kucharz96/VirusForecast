395e6d88611341d4e47459557d5b0a3e8c3831e4
Mathew <potters22@wp.pl>
2021-03-21 20:37:14 +0100

    AngularJS successful init.

---
+++ wwwroot/app/app.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular
+        .module('app', []);
+
+        app.run(["$rootScope", function ($rootScope) {
+            $rootScope.RootUrl = window.RootUrl;
+        }]);
+})();
---
+++ wwwroot/app/controllers/accountCtrl.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('accountCtrl', accountCtrl);
+
+    function accountCtrl($scope) {
+        var vm = this;
+
+        _init(window.MvcModel);
+
+        function _init(model) {
+            vm.model = model;
+            console.log(vm.model);
+        }
+
+        vm.Hallo = function (email) {
+            alert(email);
+        }
+    }
+})();
