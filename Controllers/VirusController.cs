9a84f275db2c8f7afe1480e59365512f0354e115
justyna.dabrowska <justynakamila7@gmail.com>
2021-04-11 18:24:04 +0200

    VirusController

---
+++ Controllers/VirusController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirusForecast.Controllers
+{
+    [Authorize(Roles = Models.User.DOCTOR_ROLE)]
+    public class VirusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
